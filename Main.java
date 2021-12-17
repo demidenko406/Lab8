101234b34d26a5997d4948b22864dc0c36da4e1f
kalacey412 <kalacey@yandex.com>
2021-12-17 05:40:37 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class Main {
+
+  public static void main(String[] args) {
+    ArrayList<String> list = new ArrayList<>();
+    list.add("Tired of");
+    list.add("Doing this");
+    list.add("labs");
+    list.add("Help mee");
+    System.out.println(list);
+  }
+}
