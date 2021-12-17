37843cf0c028e40016405a8c9cff37f89c5652ec
kalacey412 <kalacey@yandex.com>
2021-12-17 05:49:49 +0300

    Last commit

--- Main.java
+++ Main.java
@@ -10,6 +10,28 @@ public class Main {
     list.add("Doing this");
     list.add("labs");
     list.add("Help mee");
-    System.out.println(list);
+    ArrayList<Integer> int_list = new ArrayList<>();
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    list.add(4);
+    list.add(5);
+    list.add(13);
+    list.add(1);
+    list.add(1);
+    list.add(1);
+    list.add(1);
+    int count = 0;
+    for(int i =0;i < int_list.size();i++){
+      for(int j =0;j < int_list.size();j++){
+        if(int_list.get(i).equals(int_list.get(j))){
+          count++;
+        }
+      }
+    }
+    int_list.remove(count);
+    
+
+    
   }
 }
